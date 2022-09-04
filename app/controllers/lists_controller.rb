b249b07f87fb230414f47feb48f99610f3f2eed3
mayukan <leemy3023@gmail.com>
2022-09-04 10:18:12 +0000

    app1

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,9 +6,20 @@ class ListsController < ApplicationController
     @lists = List.all
   end
 
+  def create
+    list = List.index(list_params)
+    list.save
+    redirect_to '/top'
+  end
+
   def show
   end
 
   def edit
   end
+
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
+end
 end
