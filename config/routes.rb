29f90809311d12c5758e8e88539b50d1f4fa20ca
mayukan <leemy3023@gmail.com>
2022-09-13 22:51:51 +0000

    app1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'books/new'
   post 'books' => 'books#create'
-  get 'books/index'
-  get 'books/show/:id' => 'books#show'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show'
   get 'books/edit'
   get 'books/top'
   get '/top' => 'homes#top'
