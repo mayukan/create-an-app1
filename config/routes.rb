9ae6d1a306daaf52a4141c1d3f6be157f2272077
mayukan <leemy3023@gmail.com>
2022-09-17 07:04:58 +0000

    app1

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/edit'
-  get 'books/top'
-  get '/top' => 'homes#top'
+#  get 'books/new'
+#  get 'books' => 'books#index'
+#  post 'books' => 'books#create'
+#  get 'books/:id' => 'books#show', as: 'book'
+#  get 'books/edit'
+#  get 'books/top'
+#  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :books
 end
