f0ba0eafa15b4068df361adb624bf6e3013b5667
mayukan <leemy3023@gmail.com>
2022-09-17 09:53:56 +0000

    destroy

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 #  get 'books/top'
 #  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  patch 'books/:id' => 'books#update', as: 'update_book'
   resources :books
 end
