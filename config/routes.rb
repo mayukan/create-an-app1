1c480e14212e789cf6135dbf04d047490ed6238d
mayukan <leemy3023@gmail.com>
2022-09-20 11:05:38 +0000

    app1

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   patch 'books/:id' => 'books#update', as: 'update_book'
-  # patch 'books/:id' => 'books#update', as: 'update_book'
   resources :books
 end
