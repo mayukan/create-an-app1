89aa33c41de91925d47b6239b4338ef156701264
mayukan <leemy3023@gmail.com>
2022-09-20 11:14:50 +0000

    update-function

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 #  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  patch 'books/:id' => 'books#update', as: 'update_book'
+  # patch 'books/:id' => 'books#update', as: 'update_book'
   resources :books
 end
